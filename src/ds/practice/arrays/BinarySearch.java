1f9b92833c74a645cb55f6f78fcaddaeaff33810
Divya Spandana Marneni <DivyaSpandana.Marneni@Honeywell.com>
2018-10-07 07:29:11 -0400

    get last index with bin search

--- src/ds/practice/arrays/BinarySearch.java
+++ src/ds/practice/arrays/BinarySearch.java
@@ -1,5 +1,34 @@
 package ds.practice.arrays;
 
+import java.util.Scanner;
+
 public class BinarySearch {
 
+    public int binarySearch(int[] array, int x, int start, int last) {
+        if (array == null || array.length == 0) {
+            return-1;
+        }
+
+        int mid = (start+last)/2;
+
+        if(x == array[mid])
+            return mid;
+        else if(x > array[mid]){
+            start = mid + 1;
+        }
+        else {
+            last = mid -1;
+        }
+        return binarySearch(array, x, start, last);
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {0,12,15,16, 18, 21, 32, 56};
+        numbers = new int[] {0, 1, 2, 2, 4, 5, 5, 5, 8, 8, 8, 8, 8, 8,9 ,9,9,9,9,9,9,9,9,9};
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number to search");
+        int x = Integer.parseInt(scanner.next());
+
+        System.out.println("Binary search result: " + new BinarySearch().binarySearch(numbers, x, 0 , numbers.length-1));
+    }
 }
--- src/ds/practice/arrays/GetLastIndex.java
+++ src/ds/practice/arrays/GetLastIndex.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class GetLastIndex {
     // M1
     // reverse the array and get the first occurrence of the number
+    // complexity worst O(n)
+    // best O(1)
+    // avg O(n)
     private int getLastIndex(int[] array, int x) {
         for(int i = array.length -1; i >= 0; i--) {
             if(array[i] == x)
@@ -12,18 +15,64 @@ public class GetLastIndex {
         }
         return -1;
     }
-    // complexity worst O(n)
-    // best O(1)
-    // avg O(n)
+
+
+    // solution with binary search
+    // not ideal since we are using a for loop which is of order O(n)
+    // complexity log n + n/2 = O(n)
+    private int getLastIndexWithBinary(int[] array, int x) {
+        BinarySearch binarySearch = new BinarySearch();
+        int find = -1;
+        int firstFind = binarySearch.binarySearch(array, x, 0, array.length-1);
+        if(firstFind == -1)
+            return -1;
+        for(int i = firstFind+1; i < array.length; i ++) {
+            if(array[i] == x ) {
+                find = i;
+            }
+        }
+        return find;
+    }
+
+    // optimized
+    // O(log n)
+    private int getLastIndexBinaryUtil(int[] array, int x){
+        if(array == null || array.length == 0)
+            return -1;
+        int start = 0;
+        int last = array.length -1;
+
+        while (start <= last) {
+            int mid = (start + last)/2;
+            if(array[mid] == x && ((mid == last) || array[mid+1] > x))
+                return mid;
+            else if(x < array[mid]){
+                last = mid-1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
 
 
     public static void main(String[] args) {
-        int[] numbers = {0, 1, 2, 2, 4, 5, 5, 5, 8};
+        int[] numbers = {0, 1, 2, 2, 4, 5, 5, 5, 8, 8, 8, 8, 8, 8,9 ,9,9,9,9,9,9,9,9,9, 10, 10 , 11, 11 , 11,11,11,11};
         System.out.println("Enter the element");
         Scanner scanner = new Scanner(System.in);
         int x = Integer.parseInt(scanner.next());
-        System.out.println("The index is : " + new GetLastIndex().getLastIndex(numbers, 5));
+        long start = System.currentTimeMillis();
+        System.out.println("The index is : " + new GetLastIndex().getLastIndex(numbers, x));
+        long end = System.currentTimeMillis();
+        System.out.println("Time with reverse: " + (end - start));
+        start = System.currentTimeMillis();
+        System.out.println("Last index with binary search: " + new GetLastIndex().getLastIndexWithBinary(numbers,x));
+        end = System.currentTimeMillis();
+        System.out.println("Time with bin search: " + (end-start));
 
-
+        start = System.currentTimeMillis();
+        System.out.println("Last index with bin search: " + new GetLastIndex().getLastIndexBinaryUtil(numbers,x));
+        end = System.currentTimeMillis();
+        System.out.println("Time with bin search: " + (end-start));
     }
 }
